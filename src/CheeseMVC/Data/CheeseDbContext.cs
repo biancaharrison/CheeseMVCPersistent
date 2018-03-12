0a44cbcdd3d066ccf474f484a59687b4233fd8a7
Bianca Harrison <bdgpatriot@gmail.com>
2018-03-12 04:47:19 -0500

    completed studio part 3

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -11,5 +11,16 @@ namespace CheeseMVC.Data
             : base(options)
         { }
 
+        public DbSet<CheeseCategory> Categories { get; set; }
+
+        public DbSet<Menu> Menus { get; set; }
+
+        public DbSet<CheeseMenu> CheeseMenus { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>().HasKey(c => new { c.CheeseID, c.MenuID });
+        }
+
     }
 }
